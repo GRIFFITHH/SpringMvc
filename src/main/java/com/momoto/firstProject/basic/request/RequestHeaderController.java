070060f6fb008990d01fda6ac04430a2fc6ff83a
GRIFFITHH <glqglq032@gmail.com>
2021-09-17 16:52:05 +0900

    9/17 Initial commit

---
+++ src/main/java/com/momoto/firstProject/basic/request/RequestHeaderController.java
@@ -0,0 +1,38 @@
+package com.momoto.firstProject.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+@Slf4j
+@RestController//응답값을 view를 찾는게 아니라 문자그대로 반환 -> test용
+public class RequestHeaderController {
+
+    @RequestMapping("/headers")
+    public String headers(HttpServletRequest request,
+                          HttpServletResponse response,
+                          HttpMethod httpMethod,
+                          Locale locale,
+                          @RequestHeader("host") String host,
+                          @RequestHeader MultiValueMap<String, String> headerMap,
+                          @CookieValue(value = "myCookie", required = false) String cookie) {
+
+        log.info("request={}", request);
+        log.info("response={}", response);
+        log.info("httpMethod={}", httpMethod);
+        log.info("locale={}", locale);
+        log.info("headerMap={}", headerMap);
+        log.info("header host={}", host);
+        log.info("myCookie={}", cookie);
+        return "ok";
+
+    }
+}
